fcec27415d7364bebd530423a314ea48b9e9a268
LYLYXY <1223118063@qq.com>
2019-11-28 16:23:17 +0800

    index.js的swiper增加autoplay配置

--- src/js/index.js
+++ src/js/index.js
@@ -1,41 +1,45 @@
 /* new WOW().init(); */
 /* $('body').running(); */
 /* new Swiper('.banner .swiper-container', {
-    pagination: {
-        el: '.banner .swiper-pagination',
-        clickable: true
+  pagination: {
+    el: '.banner .swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.banner .swiper-button-next',
+    prevEl: '.banner .swiper-button-prev',
+  },
+  on: {
+    slideChangeTransitionStart: function () {
+      $('.banner ul li').eq(this.activeIndex).addClass('active').siblings().removeClass('active')
+    }
+  },
+  slidesPerView: 3,
+  slidesPerGroup: 3,
+  spaceBetween: 50,
+  slidesPerColumn: 2,
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
     },
-    navigation: {
-        nextEl: '.banner .swiper-button-next',
-        prevEl: '.banner .swiper-button-prev',
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
     },
-    on: {
-        slideChangeTransitionStart: function () {
-            $('.banner ul li').eq(this.activeIndex).addClass('active').siblings().removeClass('active')
-        }
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
     },
-    slidesPerView : 3,
-    slidesPerGroup : 3,
-    spaceBetween:50,
-    slidesPerColumn:2,
-    breakpoints: {
-        640: {
-          slidesPerView: 2,
-          spaceBetween: 20,
-        },
-        768: {
-          slidesPerView: 4,
-          spaceBetween: 40,
-        },
-        1024: {
-          slidesPerView: 5,
-          spaceBetween: 50,
-        },
-      }
+  },
+  autoplay: {
+    delay: 1000,
+    disableOnInteraction: false
+  }
 }) */
 
 $(function () {
 
-    
+
 
 })
